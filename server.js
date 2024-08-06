7558875f3b74ea39cfbab71af06883dec1f02df7
rakib-molla <rakibmail1997@gmail.com>
2024-08-06 18:18:18 +0600

    cors install

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const dbConnect = require('./dbConnection');
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 5000;
 
 // all routes
@@ -8,6 +9,7 @@ const itemsRoute = require('./routes/itemsRoute');
 
 // middleware
 app.use(express.json());
+app.use(cors());
 
 app.use('/api', itemsRoute);
 
